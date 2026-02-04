79965cc0776f62990eb50b827b02c3c9ffbcf2d3
Said Ebadi <said@maketheplay.ai>
2026-02-04 23:02:31 +0100

    First pitch! Osäker på resten av statements. Fortsätter imorgon.

--- migrations/01-said-users.sql
+++ migrations/01-said-users.sql
@@ -1,1 +1,13 @@
 
+-- =============================================
+-- USERS 5.2(2.1-2)
+-- Ansvarig: Said
+-- =============================================
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE;
+
+   -- To be continued...
