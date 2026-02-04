70b0c1e62cbfcafa19b36936f5ff487184a4da31
piensoho@gmail.com <piensoho@gmail.com>
2026-02-04 17:25:10 +0100

    minor changes to bash script so hopefully it dosnt get stuck if no sql files exists

--- scripts/mariadb-test.sh
+++ scripts/mariadb-test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 set -e 
+shopt -s nullglob
+
+files=(migrations/*.sql)
+
+if [ ${files[@]} -eq 0 ]; then
+  echo "No sql files found..."
+fi 
 
 until mariadb-admin ping -h"$MARIADB_HOST" --silent; do sleep 1; done
 
@@ -8,3 +15,6 @@ for file in migrations/*.sql; do
   echo "testing file: $file"
   mariadb -h"$MARIADB_HOST" -u root -p"$MARIADB_ROOT_PASSWORD" "$MARIADB_DATABASE" --abort-source-on-error < "$file"
 done
+
+
+
