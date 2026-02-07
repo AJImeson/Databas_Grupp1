bb94af423ef6d0dd9712fa8de8cbf25a5313d5a3
AJImeson <axel.imeson@chasacademy.se>
2026-02-07 08:45:04 +0100

    Updated some logic/mapping suggestions to warehouse

--- migrations/04-axel-orders.sql
+++ migrations/04-axel-orders.sql
@@ -1,3 +1,8 @@
+-- =============================================
+-- Orders 5.5
+-- Ansvarig: Axel
+-- =============================================
+
 -- Orders Migration Tables
 
 START TRANSACTION; 
@@ -6,8 +11,9 @@ START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS order_status (
 	status_id INTEGER NOT NULL AUTO_INCREMENT,
-	status_name VARCHAR(20) NOT NULL UNIQUE, -- Set PK as well?
-	PRIMARY KEY (status_id), -- Add status_name as PK?
+	status_name VARCHAR(20) NOT NULL UNIQUE,
+	PRIMARY KEY (status_id),
+	CONSTRAINT unique_status UNIQUE (status_name),	
 	CONSTRAINT monitor_status CHECK (status_name IN ('awaiting', 'fulfilled', 'cancelled'))
 );
 
@@ -28,14 +34,15 @@ CREATE TABLE IF NOT EXISTS orders (
 CREATE TABLE IF NOT EXISTS order_items (
 	items_id INTEGER NOT NULL AUTO_INCREMENT,
 	order_id INTEGER NOT NULL,
-	product_id INTEGER NOT NULL, --SKU in warehouse?
-	qty INTEGER NOT NULL CHECK (qty >= 0),
+	product_id INTEGER NOT NULL,
+	sku VARCHAR(50) NOT NULL -- Mapping for warehouse?
+	qty INTEGER NOT NULL CHECK (qty > 0),
 	warehouse_id INTEGER NOT NULL,
 	PRIMARY KEY (items_id),
-	CONSTRAINT distinct_complete UNIQUE (order_id, product_id, warehouse_id),
-	-- CONSTRAINT link FOREIGN KEY (warehouse_id, product_id) REFERENCES inventory(warehouse_id, SKU) ON UPDATE CASCADE, << Logic link for warehouse? 
+	CONSTRAINT distinct_complete UNIQUE (order_id, sku, warehouse_id),
 	CONSTRAINT item_order FOREIGN KEY (order_id) REFERENCES orders(order_id) ON UPDATE CASCADE,
-	CONSTRAINT item_product FOREIGN KEY (product_id) REFERENCES products (product_id) ON UPDATE CASCADE --Redundant? 
+	CONSTRAINT item_product FOREIGN KEY (product_id) REFERENCES products (product_id) ON UPDATE CASCADE, 
+	CONSTRAINT links FOREIGN KEY (warehouse_id, sku) REFERENCES warehouse(warehouse_id, sku) ON UPDATE CASCADE -- Link to Warehouse? 
 );
 
 COMMIT;
