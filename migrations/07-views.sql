c288f880d7d9a9cf4435e5908a4b30459e4cc27f
MikaSamba88 <mika.samba@chasacademy.se>
2026-02-11 12:44:08 +0100

    lagt till 3st views som kan testas

---
+++ migrations/07-views.sql
@@ -0,0 +1,44 @@
+-- ==========================================
+-- 07-VIEWS.SQL
+-- Beskrivning: Skapar vyer för lager, produktinfo och meddelanden.
+-- ==========================================
+
+-- Vy för lagerstatus
+CREATE OR REPLACE VIEW view_warehouse_stock AS
+SELECT  
+  CITY AS warehouse_city,
+  name AS product_name,
+  sku,
+  STOCK_QUANTITY
+FROM WAREHOUSES 
+JOIN INVENTORY USING (WAREHOUSE_ID)
+JOIN product_information USING (sku)
+JOIN products USING (product_id)
+;
+
+-- Vy för produktbeskrivningar
+CREATE OR REPLACE VIEW view_product_catalog AS
+SELECT
+  name, sku, selling_price, size, colour, material
+FROM products
+JOIN product_information USING (product_id)
+JOIN product_description USING (product_id)
+;
+
+-- Vy för djur ägare
+CREATE OR REPLACE VIEW view_user_pets AS
+SELECT
+  username AS owner_name,
+  given_name AS per_name,
+  common_name AS species,
+  is_alive
+FROM users
+JOIN pets USING (user_id)
+JOIN species USING (species_id)
+;
+
+
+-- TEST-QUERIES (Kör dessa gör att se resultat)
+-- SELECT * FROM view_warehouse_stock;
+-- SELECT * FROM view_product_catalog;
+-- SELECT * FROM view_user_pets;
