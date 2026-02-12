df8eb29035720084129ebc4dc803305c0f49e447
AJImeson <axel.imeson@chasacademy.se>
2026-02-12 14:14:23 +0100

    Mapping to warehouse, fixed value iterations

--- migrations/04-axel-orders.sql
+++ migrations/04-axel-orders.sql
@@ -44,14 +44,14 @@ CREATE TABLE IF NOT EXISTS order_items (
     items_id INTEGER NOT NULL AUTO_INCREMENT,
     order_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
-    sku VARCHAR(50) NOT NULL,
+    sku VARCHAR(8) NOT NULL,
     qty INTEGER NOT NULL CHECK (qty > 0),
     warehouse_id INTEGER NOT NULL,
     sale_price DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (items_id),
     CONSTRAINT link_order FOREIGN KEY (order_id) REFERENCES orders(order_id),
     CONSTRAINT link_shop FOREIGN KEY (product_id) REFERENCES products(product_id)
-    -- CONSTRAINT link_warehouse FOREIGN KEY (warehouse_id, sku) REFERENCES inventory(warehouse_id, sku) -- Warehouse table has in uppercase, should be converted to lowercase 
+    CONSTRAINT link_warehouse FOREIGN KEY (warehouse_id, sku) REFERENCES inventory(warehouse_id, sku) ON UPDATE CASCADE
 );
 COMMIT;
 
@@ -64,21 +64,19 @@ COMMIT;
  -- Dummy data for orders 
 SET autocommit = 0;
 INSERT INTO orders (user_id, status_id, shipping_address_id) VALUES
-(1, 1, 10), -- Awaiting
-(2, 2, 11), -- Fulfilled
-(3, 3, 12), -- Cancelled
-(4, 2, 13),
-(5, 2, 14),
-(6, 3, 15),
-(7, 1, 16);
+(1, 1, 1), -- Awaiting
+(2, 2, 2), -- Fulfilled
+(3, 3, 3), -- Cancelled
+(4, 2, 4),
+(5, 1, 5);
 COMMIT;
 
 -- Dummy data for Items
 SET autocommit = 0;
 INSERT INTO order_items (order_id, product_id, qty, warehouse_id, sale_price) VALUES
-(1, 001, 4, 1, 35,99),
-(1, 002, 4, 1, 39,99),
-(1, 003, 4, 1, 42,99);
+(1, 1, 4, 1, 35.99),
+(1, 2, 4, 1, 39.99),
+(1, 3, 4, 1, 42.99);
 COMMIT;
 
 
