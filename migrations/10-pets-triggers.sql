5b6eb9ffa793db961d316740d4c0363ef63d01c9
AJImeson <axel.imeson@chasacademy.se>
2026-02-19 10:16:36 +0100

    Fixed Pets triggers

--- migrations/10-pets-triggers.sql
+++ migrations/10-pets-triggers.sql
@@ -1,23 +1,33 @@
-USE ace_ventura;
+-- Pets
 
+USE ace_ventura;
 DELIMITER //
 
--- Species Triggers
+-- Species ID Creation 
 
 -- Insert
 CREATE TRIGGER species_insert
 AFTER INSERT ON species
+FOR EACH ROW	
+BEGIN
+    INSERT INTO pets_activities (pets_id, modification, modified_value, changed_by)
+    VALUES (NEW.species_id, 'INSERT', 'New Species Record Created', USER());
+END; //
+
+-- Species Name Creation 
+
+-- Insert
+CREATE TRIGGER species_common_name_insert
+AFTER INSERT ON species_common_names
 FOR EACH ROW
 BEGIN
-    -- Species data 
     INSERT INTO pets_activities (pets_id, modification, modified_value, changed_by)
     VALUES (NEW.species_id, 'INSERT', NEW.common_name, USER());
 END; //
 
 -- Pets Triggers
 
--- Insert
-
+-- Insert 
 CREATE TRIGGER pets_insert
 AFTER INSERT ON pets
 FOR EACH ROW
@@ -27,24 +37,23 @@ BEGIN
 END; //
 
 -- Update
-
 CREATE TRIGGER pets_update
 AFTER UPDATE ON pets
 FOR EACH ROW
 BEGIN
-    -- Name Change
+    -- Name
     IF OLD.given_name <> NEW.given_name THEN
         INSERT INTO pets_activities (pets_id, modification, former_value, modified_value, changed_by)
         VALUES (OLD.pet_id, 'UPDATE', OLD.given_name, NEW.given_name, USER());
     END IF;
 
-    -- Owner (User)
+    -- Owner 
     IF OLD.user_id <> NEW.user_id THEN
         INSERT INTO pets_activities (pets_id, modification, former_value, modified_value, changed_by)
         VALUES (OLD.pet_id, 'UPDATE', CAST(OLD.user_id AS CHAR), CAST(NEW.user_id AS CHAR), USER());
     END IF;
 
-    -- Status (Alive/Dead)
+    -- Status 
     IF OLD.is_alive <> NEW.is_alive THEN
         INSERT INTO pets_activities (pets_id, modification, former_value, modified_value, changed_by)
         VALUES (OLD.pet_id, 'UPDATE', IF(OLD.is_alive, '1', '0'), IF(NEW.is_alive, '1', '0'), USER());
